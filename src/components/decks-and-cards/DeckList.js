ae1d0e51f942fe7076b39c2373e0c8e69a7a5bc0
thanghq99 <haikhauak@gmail.com>
2021-12-28 16:52:07 +0700

    home page pagination

--- src/components/decks-and-cards/DeckList.js
+++ src/components/decks-and-cards/DeckList.js
@@ -1,6 +1,93 @@
 import React, { useState, useEffect, useContext } from 'react';
+import ReactPaginate from 'react-paginate';
 import { firebaseAuth } from '../../provider/AuthProvider';
 import SelectableDeck from './SelectableDeck';
+
+function Items({ currentItems, user, setDeckToEdit}) {
+  return (
+    <React.Fragment>
+      <div>
+      {currentItems && 
+        <div className="deck-list">
+        <ul>
+          {currentItems.length > 0 ? 
+            currentItems.map((deck, index) => {
+              return (
+                <SelectableDeck 
+                  key={index}
+                  // toggleDeck={toggleDeck}
+                  // selectedDecks={selectedDecks}
+                  length={currentItems.numCards}
+                  // setSelectedDecks={setSelectedDecks}
+                  deck={deck}
+                  mine={user && currentItems.owner == currentItems.uid}
+                  setDeckToEdit={() => {
+                    setDeckToEdit({ id: currentItems.id, title: currentItems.title, private: currentItems.private, description: currentItems.description });
+                  }}
+                />
+              );}
+            )
+          :
+            <p>セットがありません。 作成して開始してください！</p>
+          }
+        </ul>
+      </div>
+      }
+      </div>
+    
+    </React.Fragment>
+  );
+}
+
+function PaginatedItems({ itemsPerPage, decks, user, setDeckToEdit}) {
+  // We start with an empty list of items.
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(decks.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(decks.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = event.selected * itemsPerPage % decks.length;
+    console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Items currentItems={currentItems} user={user} />
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+}
 
 const DeckList = ({
   decks,
@@ -43,13 +130,14 @@ const DeckList = ({
 
   return (
     <div className="deck-list">
-      <ul>
+      {/* <ul>
         {deckList.length > 0 ? 
           deckList
         :
           <p>セットがありません。 作成して開始してください！</p>
         }
-      </ul>
+      </ul> */}
+      <PaginatedItems itemsPerPage={4} decks={decks} user={user} setDeckToEdit={setDeckToEdit}/>
     </div>
   );
 }
