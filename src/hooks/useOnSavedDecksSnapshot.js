078aedc5ad973b4bafc708178d6f3699c4c25d24
thanghq99 <haikhauak@gmail.com>
2022-01-19 22:52:34 +0700

    add deck saved users count

--- src/hooks/useOnSavedDecksSnapshot.js
+++ src/hooks/useOnSavedDecksSnapshot.js
@@ -17,6 +17,7 @@ const useOnSavedDecksSnapshot = (user) => {
       snapshot.forEach(deck => {
         let data = {
           id: deck.id,
+          saveCount: deck.data().saved_users.length,
           ...deck.data()
         };
         if (data.saved_users?.includes(user.uid)) {
